f22943885c3656957f3bb660f6620836cbfbaa9c
주딘 <joo6077@gmail.com>
2021-05-11 15:51:44 +0900

    tween animationbuilder

--- ninja_trips/lib/shared/screenTitle.dart
+++ ninja_trips/lib/shared/screenTitle.dart
@@ -7,9 +7,24 @@ class ScreenTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+    return TweenAnimationBuilder(
+      duration: Duration(milliseconds: 500),
+      tween: Tween<double>(
+        begin: 0,
+        end: 1,
+      ),
+      // (context, tween, child)
+      builder: (BuildContext context, double _val, Widget child) {
+        return Opacity(
+          opacity: _val,
+          child: Padding(padding: EdgeInsets.only(top: _val * 20), child: child,),
+        );
+      },
+      child: Text(
+        text,
+        style: TextStyle(
+            fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
